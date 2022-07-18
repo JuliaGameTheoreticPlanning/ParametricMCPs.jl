fda4e73322d8c405f3c56ae03b554e5a01719a05
lassepe <lasse.peters@mailbox.org>
2022-07-18 22:46:05 +0200

    Simplify active set computation and fix indexing bug

--- src/autodiff.jl
+++ src/autodiff.jl
@@ -3,18 +3,9 @@ function _solve_jacobian_θ(problem, solution, θ; active_tolerance = 1e-3)
     z_star = solution.z
 
     active_indices = let
-        # if zᵢ is unbouded on both sides, Fᵢ(z, θ) = 0 is always active
-        unbounded_indices = findall(isinf.(lower_bounds) .& isinf.(upper_bounds))
-        # for the remainig indices, we have to check the solution
-        bounded_indices = setdiff(1:get_problem_size(problem), unbounded_indices)
-
-        @assert all(>=(2 * active_tolerance), upper_bounds - lower_bounds)
-        strongly_active_indices = filter(bounded_indices) do ii
-            z_star[ii] >= lower_bounds[ii] + active_tolerance &&
-                z_star[ii] <= upper_bounds[ii] - active_tolerance
-        end
-
-        [unbounded_indices; strongly_active_indices]
+        lower_active = z_star .>= (lower_bounds .+ active_tolerance)
+        upper_active = z_star .<= (upper_bounds .- active_tolerance)
+        findall(lower_active .& upper_active)
     end
 
     ∂z∂θ = SparseArrays.spzeros(get_problem_size(problem), get_parameter_dimension(problem))
@@ -34,8 +25,8 @@ function _solve_jacobian_θ(problem, solution, θ; active_tolerance = 1e-3)
         ∂f∂z[active_indices, active_indices]
     end
 
-    ∂z∂θ[active_indices, active_indices] = LinearAlgebra.qr(-∂f_reduced∂z_reduced) \ ∂f_reduce∂θ
-    ∂z∂θ
+    ∂z∂θ[active_indices, :] = LinearAlgebra.qr(-∂f_reduced∂z_reduced) \ collect(∂f_reduce∂θ)
+    (; ∂z∂θ, active_indices)
 end
 
 function ChainRulesCore.rrule(::typeof(solve), problem, θ; kwargs...)
