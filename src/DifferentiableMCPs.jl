50f79eeafe02bce1995151132c94fd696a7a9ad2
lassepe <lasse.peters@mailbox.org>
2022-07-19 00:53:24 +0200

    Add missing dependency on LinearAlgebra

--- src/DifferentiableMCPs.jl
+++ src/DifferentiableMCPs.jl
@@ -1,9 +1,10 @@
 module DifferentiableMCPs
 
-using Symbolics: Symbolics
-using SparseArrays: SparseArrays
-using PATHSolver: PATHSolver
 using ChainRulesCore: ChainRulesCore
+using LinearAlgebra: LinearAlgebra
+using PATHSolver: PATHSolver
+using SparseArrays: SparseArrays
+using Symbolics: Symbolics
 
 include("sparse_utils.jl")
 include("parametric_problem.jl")
