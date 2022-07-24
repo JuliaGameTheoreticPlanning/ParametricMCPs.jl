e4f570ca5b49dce1cb0732b980611118486959d0
lassepe <lasse.peters@mailbox.org>
2022-07-24 15:22:22 +0200

    Use lazy vcat to reduce allocations

--- src/DifferentiableMCPs.jl
+++ src/DifferentiableMCPs.jl
@@ -5,16 +5,13 @@ using LinearAlgebra: LinearAlgebra
 using PATHSolver: PATHSolver
 using SparseArrays: SparseArrays
 using Symbolics: Symbolics
-
+using LazyArrays: LazyArrays
 
 include("sparse_utils.jl")
-
 include("parametric_problem.jl")
 export ParametricMCP, get_parameter_dimension, get_problem_size
 include("solver.jl")
 export solve
-
 include("autodiff.jl")
-
 
 end
