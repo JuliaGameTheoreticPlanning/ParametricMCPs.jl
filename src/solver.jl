0526157d5a75568ca3b353cacb1452b2c9a63a64
lassepe <lasse.peters@mailbox.org>
2023-02-23 11:48:42 +0100

    Even more agressive caching

--- src/solver.jl
+++ src/solver.jl
@@ -15,10 +15,9 @@ function solve(
         Cint(0)
     end
 
-    result = get_result_buffer(jacobian_z!)
     function J(n, nnz, z, col, len, row, data)
-        jacobian_z!(result, z, θ)
-        _coo_from_sparse!(col, len, row, data, result)
+        jacobian_z!(jacobian_z!.result_buffer, z, θ)
+        _coo_from_sparse!(col, len, row, data, jacobian_z!.result_buffer)
         Cint(0)
     end
 
--- src/sparse_utils.jl
+++ src/sparse_utils.jl
@@ -1,22 +1,28 @@
-struct SparseFunction{T}
-    _f::T
+struct SparseFunction{T1,T2}
+    _f::T1
+    result_buffer::T2
     rows::Vector{Int}
     cols::Vector{Int}
     size::Tuple{Int,Int}
     constant_entries::Vector{Int}
-    function SparseFunction(_f::T, rows, cols, size, constant_entries = Int[]) where {T}
+    function SparseFunction(_f::T1, rows, cols, size, constant_entries = Int[]) where {T1}
         length(constant_entries) <= length(rows) ||
             throw(ArgumentError("More constant entries than non-zero entries."))
-        new{T}(_f, rows, cols, size, constant_entries)
+        result_buffer = get_result_buffer(rows, cols, size)
+        new{T1,typeof(result_buffer)}(_f, result_buffer, rows, cols, size, constant_entries)
     end
 end
 
 (f::SparseFunction)(args...) = f._f(args...)
 SparseArrays.nnz(f::SparseFunction) = length(f.rows)
 
+function get_result_buffer(rows::Vector{Int}, cols::Vector{Int}, size::Tuple{Int,Int})
+    data = zeros(length(rows))
+    SparseArrays.sparse(rows, cols, data, size...)
+end
+
 function get_result_buffer(f::SparseFunction)
-    data = zeros(SparseArrays.nnz(f))
-    SparseArrays.sparse(f.rows, f.cols, data, f.size...)
+    get_result_buffer(f.rows, f.cols, f.size)
 end
 
 """
