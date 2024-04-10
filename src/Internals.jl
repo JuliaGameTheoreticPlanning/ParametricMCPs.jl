57c92d0e8ee3f9f75ec0ad8d618d920d557786eb
lassepe <lasse.peters@mailbox.org>
2024-04-10 13:53:46 +0200

    Support for FastDifferentiation.jl

---
+++ src/Internals.jl
@@ -0,0 +1,42 @@
+module Internals
+
+using FastDifferentiation: FastDifferentiation as FD
+
+function parameterize(f::Vector, θ)
+    f
+end
+
+function parameterize(f, θ)
+    f(θ)
+end
+
+function infer_problem_size(lower_bounds, upper_bounds)
+    if lower_bounds isa AbstractVector
+        length(lower_bounds)
+    elseif upper_bounds isa AbstractVector
+        length(upper_bounds)
+    else
+        throw(
+            ArgumentError(
+                "Cannot infer problem size from lower_bounds and upper_bounds. Provide it explicitly.",
+            ),
+        )
+    end
+end
+
+function check_dimensions(args...)
+    function to_dimension(arg)
+        if arg isa AbstractVector
+            length(arg)
+        elseif arg isa Int
+            arg
+        else
+            throw(ArgumentError("Expected an AbstractVector or an Int."))
+        end
+    end
+
+    the_unique_dimension = (to_dimension(arg) for arg in args) |> unique |> only
+    the_unique_dimension
+end
+
+end
--- src/parametric_problem.jl
+++ src/parametric_problem.jl
@@ -2,6 +2,28 @@
 ParametricMCP represents a mixed complementarity problem (MCP) parameterized by some vector `θ`.
 
 Solutions of this problem solve f(z, θ) ⟂ lb ≤ z ≤ ub.
+
+--------------------------------------------------------------------------------
+
+The main constructor for compiling a `ParametricMCP` from
+
+Positional arguments:
+- `f`: callable as `f(z, θ)` that maps a length `n` vector of decision variables `z` and a \
+parameter vector `θ` of size `parameter_dimension` to an length `n` vector output.
+- `lower_bounds`: A length `n` vector of element-wise lower bounds on the decision variables `z`.
+- `upper_bounds`: A length `n` vector of element-wise upper bounds on the decision variables `z`.
+- `parameter_dimension`: the size of the parameter vector `θ` in `f`.
+
+Keyword arguments:
+- `[backend]`: the backend to be used for compiling callbacks for `f` and its Jacobians needed by PATH. `SymbolicsBackend` (default) is slightly more flexible. `FastDifferentationBackend` has reduced compilation times and reduced runtime in some cases.
+- `compute_sensitivities`: whether to compile the callbacks needed for sensitivity computation.
+- `[problem_size]`: the number of decision variables. If not provided and `lower_bounds` or `upper_bounds` are vectors, the problem size is inferred from the length of these vectors.
+
+Note, this constructor uses `Symbolics.jl` to compile the relevant low-level functions. Therefore,
+`f` must be implemented in a sufficiently generic way that supports symbolic evaluation. In cases
+where that is strictly infeasible, you can still use the low-level constructor to generate a
+`ParametricMCP`. In general, however, the use of this convenience constructor is advised.
+
 """
 struct ParametricMCP{T1,T2,T3}
     "A callable `f!(result, z, θ)` which stores the result of `f(z, θ)` in `result`"
@@ -16,35 +38,44 @@ struct ParametricMCP{T1,T2,T3}
     upper_bounds::Vector{Float64}
     "The number of 'runtime'-parameters of the problem."
     parameter_dimension::Int
+    "The number of decision variables of the problem."
+    problem_size::Int
 end
 
 "Returns the number of decision variables for this problem."
-get_problem_size(problem::ParametricMCP) = length(problem.lower_bounds)
+get_problem_size(problem::ParametricMCP) = problem.problem_size
 
 "Returns the number of decision variables for this problem."
 get_parameter_dimension(problem::ParametricMCP) = problem.parameter_dimension
 
-"""
-The main constructor for compiling a `ParametricMCP` from
+struct SymbolicsBackend end
+struct FastDifferentationBackend end
 
-- `f`: callabale as `f(z, θ)` that maps a lenght `n` vector of decision variables `z` and a \
-parameter vector `θ` of size `parameter_dimension` to an lenght `n` vector output.
-- `lower_bounds`: A lenght `n` vector of element-wise lower bounds on the decision variables `z`.
-- `upper_bounds`: A length `n` vector of element-wise upper bounds on the decision variables `z`.
-- `parameter_dimension`: the size of the parameter vector `θ` in `f`.
+# This method allows the user to specify the backend via a keyword argument which defaults to using Symbolics.jl
+function ParametricMCP(
+    f,
+    lower_bounds,
+    upper_bounds,
+    parameter_dimension;
+    backend = SymbolicsBackend(),
+    kwargs...,
+)
+    ParametricMCP(f, backend, lower_bounds, upper_bounds, parameter_dimension; kwargs...)
+end
 
-Note, this constructor uses `Symbolics.jl` to compile the relevant low-level functions. Therefore,
-`f` must be implemented in a sufficiently generic way that supports symbolic evaluation. In cases
-where that is strictly infeasible, you can still use the low-level constructor to generate a
-`ParametricMCP`. In general, however, the use of this convenience construtor is advised.
-"""
-function ParametricMCP(f, lower_bounds, upper_bounds, parameter_dimension; kwargs...)
-    # TODO
-    length(lower_bounds) == length(upper_bounds) ||
-        throw(ArgumentError("lower_bounds and upper_bounds have inconsistent lenghts."))
-    problem_size = length(lower_bounds)
+# Dispatch for using the Symbolics.jl backend
+function ParametricMCP(
+    f,
+    ::SymbolicsBackend,
+    lower_bounds,
+    upper_bounds,
+    parameter_dimension;
+    problem_size = Internals.infer_problem_size(lower_bounds, upper_bounds),
+    kwargs...,
+)
+    problem_size = Internals.check_dimensions(lower_bounds, upper_bounds, problem_size)
 
-    # setup the problem symblically
+    # setup the problem symbolically
     z_symbolic, θ_symbolic =
         Symbolics.@variables(z_symbolic[1:problem_size], θ_symbolic[1:parameter_dimension]) .|>
         Symbolics.scalarize
@@ -58,26 +89,101 @@ function ParametricMCP(f, lower_bounds, upper_bounds, parameter_dimension; kwarg
     ParametricMCP(f_symbolic, z_symbolic, θ_symbolic, lower_bounds, upper_bounds; kwargs...)
 end
 
+function ParametricMCP(
+    f,
+    ::FastDifferentationBackend,
+    lower_bounds,
+    upper_bounds,
+    parameter_dimension;
+    problem_size = Internals.infer_problem_size(lower_bounds, upper_bounds),
+)
+    Internals.check_dimensions(lower_bounds, upper_bounds, problem_size)
+
+    # setup the problem symbolically
+    z_node = FD.make_variables(:z, problem_size)
+    θ_node = FD.make_variables(:θ, parameter_dimension)
+    f_node = f(z_node, θ_node)
+
+    ParametricMCP(f_node, z_node, θ_node, lower_bounds, upper_bounds; compute_sensitivities)
+end
+
 """
-Symbolic version of the ParmetricMCP constructor. If you have `f` and `z` already in terms of symbolic variables, use this.
+FastDifferentation.jl version of the ParmetricMCP constructor. If you have `f` and `z` already in terms of `FastDifferentation.Node`, use this.
+
+Dev notes:
+- This may become the new default back-end since it promises to be faster than Symbolics.jl. (both in terms of compilation/code-gen time and execution time).
+- We may be able to convert the Symbolics.jl representation in the future via `FDConversion.jl`
 """
 function ParametricMCP(
-    f_symbolic::Vector{Symbolics.Num},
-    z_symbolic::Vector{Symbolics.Num},
-    θ_symbolic::Vector{Symbolics.Num},
+    f::Vector{T},
+    z::Vector{T},
+    θ::Vector{T},
+    lower_bounds::Vector,
+    upper_bounds::Vector;
+    compute_sensitivities = true,
+) where {T<:FD.Node}
+    problem_size = Internals.check_dimensions(f, z, lower_bounds, upper_bounds)
+
+    # compile all the symbolic expressions into callable julia code
+    f! = let
+        # The multi-arg version of `make_function` is broken so we concatenate to a single arg here
+        _f! = FD.make_function(f, [z; θ]; in_place = true)
+        (result, z, θ) -> _f!(result, [z; θ])
+    end
+
+    # same as above but for the Jacobian in z
+    jacobian_z! = let
+        jacobian_z = FD.sparse_jacobian(f, z)
+        _jacobian_z! = FD.make_function(jacobian_z, [z; θ]; in_place = true)
+        rows, cols, _ = SparseArrays.findnz(jacobian_z)
+        # TODO: constant entry detection
+        constant_entries = get_constant_entries(jacobian_z, z)
+        SparseFunction(rows, cols, size(jacobian_z), constant_entries) do result, z, θ
+            _jacobian_z!(result, [z; θ])
+        end
+    end
+
+    if compute_sensitivities
+        jacobian_θ! = let
+            jacobian_θ = FD.sparse_jacobian(f, θ)
+            _jacobian_θ! = FD.make_function(jacobian_θ, [z; θ]; in_place = true)
+            rows, cols, _ = SparseArrays.findnz(jacobian_θ)
+            # TODO: constant entry detection
+            constant_entries = get_constant_entries(jacobian_θ, θ)
+            SparseFunction(rows, cols, size(jacobian_θ), constant_entries) do result, z, θ
+                _jacobian_θ!(result, [z; θ])
+            end
+        end
+    else
+        jacobian_θ! = nothing
+    end
+
+    parameter_dimension = length(θ)
+    ParametricMCP(
+        f!,
+        jacobian_z!,
+        jacobian_θ!,
+        lower_bounds,
+        upper_bounds,
+        parameter_dimension,
+        problem_size,
+    )
+end
+
+"""
+Symbolics.jl version of the ParmetricMCP constructor. If you have `f` and `z` already in terms of symbolic variables, use this.
+"""
+function ParametricMCP(
+    f_symbolic::Vector{<:Symbolics.Num},
+    z_symbolic::Vector{<:Symbolics.Num},
+    θ_symbolic::Vector{<:Symbolics.Num},
     lower_bounds,
     upper_bounds;
     compute_sensitivities = true,
     parallel = Symbolics.ShardedForm(),
 )
-    length(lower_bounds) == length(upper_bounds) ||
-        throw(ArgumentError("lower_bounds and upper_bounds have inconsistent lenghts."))
-    problem_size = length(lower_bounds)
-    length(f_symbolic) == problem_size || throw(
-        ArgumentError(
-            "The output lenght of `f` is inconsistent with `lower_bounds` and `upper_bounds`.",
-        ),
-    )
+    problem_size = Internals.check_dimensions(f_symbolic, z_symbolic, lower_bounds, upper_bounds)
+
     jacobian_z_symbolic = Symbolics.sparsejacobian(f_symbolic, z_symbolic)
     jacobian_θ_symbolic = Symbolics.sparsejacobian(f_symbolic, θ_symbolic)
 
@@ -116,7 +222,8 @@ function ParametricMCP(
                 parallel,
             )[2]
             rows, cols, _ = SparseArrays.findnz(jacobian_θ_symbolic)
-            SparseFunction(rows, cols, size(jacobian_θ_symbolic)) do result, z, θ
+            constant_entries = get_constant_entries(jacobian_θ_symbolic, θ_symbolic)
+            SparseFunction(rows, cols, size(jacobian_θ_symbolic), constant_entries) do result, z, θ
                 _f!(result, [z; θ])
             end
         end
@@ -125,5 +232,30 @@ function ParametricMCP(
     end
 
     parameter_dimension = length(θ_symbolic)
-    ParametricMCP(f!, jacobian_z!, jacobian_θ!, lower_bounds, upper_bounds, parameter_dimension)
+    ParametricMCP(
+        f!,
+        jacobian_z!,
+        jacobian_θ!,
+        lower_bounds,
+        upper_bounds,
+        parameter_dimension,
+        problem_size,
+    )
+end
+
+"""
+Call all functions with a dummy input to trigger JIT compilation.
+"""
+function compile_callbacks(mcp::ParametricMCP)
+    # TODO: if all callbacks are type stable, we could also use `precompile` here
+    θ = zeros(get_parameter_dimension(mcp))
+    z = zeros(get_problem_size(mcp))
+    mcp.f!(z, z, θ)
+    jacz = ParametricMCPs.get_result_buffer(mcp.jacobian_z!)
+    mcp.jacobian_z!(jacz, z, θ)
+    if !isnothing(mcp.jacobian_θ!)
+        jacθ = ParametricMCPs.get_result_buffer(mcp.jacobian_θ!)
+        mcp.jacobian_θ!(jacθ, z, θ)
+    end
+    nothing
 end
