31386030371f2dc9c51ff998fa2ee78539ead9db
lassepe <lasse.peters@mailbox.org>
2024-04-16 09:58:38 +0200

    Add tests

--- src/parametric_problem.jl
+++ src/parametric_problem.jl
@@ -54,87 +54,47 @@ function ParametricMCP(
     lower_bounds,
     upper_bounds,
     parameter_dimension;
-    backend = SymbolicsBackend(),
-    kwargs...,
-)
-    ParametricMCP(f, backend, lower_bounds, upper_bounds, parameter_dimension; kwargs...)
-end
-
-# Dispatch for using the Symbolics.jl backend
-function ParametricMCP(
-    f,
-    ::SymbolicsBackend,
-    lower_bounds,
-    upper_bounds,
-    parameter_dimension;
+    backend = SymbolicUtils.SymbolicsBackend(),
     problem_size = Internals.infer_problem_size(lower_bounds, upper_bounds),
     kwargs...,
 )
     problem_size = Internals.check_dimensions(lower_bounds, upper_bounds, problem_size)
 
-    # setup the problem symbolically
-    z_symbolic, θ_symbolic =
-        Symbolics.@variables(z_symbolic[1:problem_size], θ_symbolic[1:parameter_dimension]) .|>
-        Symbolics.scalarize
-
-    if isempty(θ_symbolic)
-        θ_symbolic = Symbolics.Num[]
-    end
-
+    z_symbolic = SymbolicUtils.make_variables(backend, :z, problem_size)
+    θ_symbolic = SymbolicUtils.make_variables(backend, :θ, parameter_dimension)
     f_symbolic = f(z_symbolic, θ_symbolic)
 
     ParametricMCP(f_symbolic, z_symbolic, θ_symbolic, lower_bounds, upper_bounds; kwargs...)
 end
 
-function ParametricMCP(
-    f,
-    ::FastDifferentationBackend,
-    lower_bounds,
-    upper_bounds,
-    parameter_dimension;
-    problem_size = Internals.infer_problem_size(lower_bounds, upper_bounds),
-)
-    Internals.check_dimensions(lower_bounds, upper_bounds, problem_size)
-
-    # setup the problem symbolically
-    z_node = FD.make_variables(:z, problem_size)
-    θ_node = FD.make_variables(:θ, parameter_dimension)
-    f_node = f(z_node, θ_node)
-
-    ParametricMCP(f_node, z_node, θ_node, lower_bounds, upper_bounds; compute_sensitivities)
-end
-
 """
-FastDifferentation.jl version of the ParmetricMCP constructor. If you have `f` and `z` already in terms of `FastDifferentation.Node`, use this.
-
-Dev notes:
-- This may become the new default back-end since it promises to be faster than Symbolics.jl. (both in terms of compilation/code-gen time and execution time).
-- We may be able to convert the Symbolics.jl representation in the future via `FDConversion.jl`
+Symbolic version of the ParmetricMCP constructor. If you have `f` and `z` already in terms of symbolic variables, use this.
 """
 function ParametricMCP(
-    f::Vector{T},
-    z::Vector{T},
-    θ::Vector{T},
+    f_symbolic::Vector{T},
+    z_symbolic::Vector{T},
+    θ_symbolic::Vector{T},
     lower_bounds::Vector,
     upper_bounds::Vector;
     compute_sensitivities = true,
-) where {T<:FD.Node}
-    problem_size = Internals.check_dimensions(f, z, lower_bounds, upper_bounds)
+) where {T<:Union{FD.Node,Symbolics.Num}}
+    problem_size = Internals.check_dimensions(f_symbolic, z_symbolic, lower_bounds, upper_bounds)
 
     # compile all the symbolic expressions into callable julia code
     f! = let
         # The multi-arg version of `make_function` is broken so we concatenate to a single arg here
-        _f! = FD.make_function(f, [z; θ]; in_place = true)
+        _f! = SymbolicUtils.build_function(f_symbolic, [z_symbolic; θ_symbolic]; in_place = true)
         (result, z, θ) -> _f!(result, [z; θ])
     end
 
     # same as above but for the Jacobian in z
     jacobian_z! = let
-        jacobian_z = FD.sparse_jacobian(f, z)
-        _jacobian_z! = FD.make_function(jacobian_z, [z; θ]; in_place = true)
+        jacobian_z = SymbolicUtils.sparse_jacobian(f_symbolic, z_symbolic)
+        _jacobian_z! =
+            SymbolicUtils.build_function(jacobian_z, [z_symbolic; θ_symbolic]; in_place = true)
         rows, cols, _ = SparseArrays.findnz(jacobian_z)
         # TODO: constant entry detection
-        constant_entries = get_constant_entries(jacobian_z, z)
+        constant_entries = get_constant_entries(jacobian_z, z_symbolic)
         SparseFunction(rows, cols, size(jacobian_z), constant_entries) do result, z, θ
             _jacobian_z!(result, [z; θ])
         end
@@ -142,86 +102,17 @@ function ParametricMCP(
 
     if compute_sensitivities
         jacobian_θ! = let
-            jacobian_θ = FD.sparse_jacobian(f, θ)
-            _jacobian_θ! = FD.make_function(jacobian_θ, [z; θ]; in_place = true)
+            jacobian_θ = SymbolicUtils.sparse_jacobian(f_symbolic, θ_symbolic)
+            _jacobian_θ! = SymbolicUtils.build_function(
+                jacobian_θ,
+                [z_symbolic; θ_symbolic];
+                in_place = true,
+            )
             rows, cols, _ = SparseArrays.findnz(jacobian_θ)
             # TODO: constant entry detection
-            constant_entries = get_constant_entries(jacobian_θ, θ)
+            constant_entries = get_constant_entries(jacobian_θ, θ_symbolic)
             SparseFunction(rows, cols, size(jacobian_θ), constant_entries) do result, z, θ
                 _jacobian_θ!(result, [z; θ])
-            end
-        end
-    else
-        jacobian_θ! = nothing
-    end
-
-    parameter_dimension = length(θ)
-    ParametricMCP(
-        f!,
-        jacobian_z!,
-        jacobian_θ!,
-        lower_bounds,
-        upper_bounds,
-        parameter_dimension,
-        problem_size,
-    )
-end
-
-"""
-Symbolics.jl version of the ParmetricMCP constructor. If you have `f` and `z` already in terms of symbolic variables, use this.
-"""
-function ParametricMCP(
-    f_symbolic::Vector{<:Symbolics.Num},
-    z_symbolic::Vector{<:Symbolics.Num},
-    θ_symbolic::Vector{<:Symbolics.Num},
-    lower_bounds,
-    upper_bounds;
-    compute_sensitivities = true,
-    parallel = Symbolics.ShardedForm(),
-)
-    problem_size = Internals.check_dimensions(f_symbolic, z_symbolic, lower_bounds, upper_bounds)
-
-    jacobian_z_symbolic = Symbolics.sparsejacobian(f_symbolic, z_symbolic)
-    jacobian_θ_symbolic = Symbolics.sparsejacobian(f_symbolic, θ_symbolic)
-
-    # compile all the symbolic expressions into callable julia code
-    f! = let
-        _f! = Symbolics.build_function(
-            f_symbolic,
-            [z_symbolic; θ_symbolic];
-            expression = Val{false},
-            parallel,
-        )[2]
-        (result, z, θ) -> _f!(result, [z; θ])
-    end
-
-    jacobian_z! = let
-        _f! = Symbolics.build_function(
-            jacobian_z_symbolic,
-            [z_symbolic; θ_symbolic];
-            expression = Val{false},
-            parallel,
-        )[2]
-        rows, cols, _ = SparseArrays.findnz(jacobian_z_symbolic)
-
-        constant_entries = get_constant_entries(jacobian_z_symbolic, z_symbolic)
-        SparseFunction(rows, cols, size(jacobian_z_symbolic), constant_entries) do result, z, θ
-            _f!(result, [z; θ])
-        end
-    end
-
-    if compute_sensitivities
-        jacobian_θ! = let
-            _f! = Symbolics.build_function(
-                jacobian_θ_symbolic,
-                [z_symbolic; θ_symbolic];
-                expression = Val{false},
-                parallel,
-            )[2]
-            rows, cols, _ = SparseArrays.findnz(jacobian_θ_symbolic)
-            constant_entries = get_constant_entries(jacobian_θ_symbolic, θ_symbolic)
-            SparseFunction(rows, cols, size(jacobian_θ_symbolic), constant_entries) do result, z, θ
-                _f!(result, [z; θ])
             end
         end
     else
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,53 +7,55 @@ using FiniteDiff: FiniteDiff
 using Symbolics: Symbolics
 
 @testset "ParametricMCPs.jl" begin
-    rng = Random.MersenneTwister(1)
-    parameter_dimension = 2
-    # setup a dummy MCP which represents a QP with:
-    # - cost: sum((z[1:2] - θ).^2)
-    # - constaints: z[1:2] >= 0
-    f(z, θ) = [2z[1:2] - z[3:4] - 2θ; z[1:2]]
-    lower_bounds = [-Inf, -Inf, 0, 0]
-    upper_bounds = [Inf, Inf, Inf, Inf]
-    problem = ParametricMCPs.ParametricMCP(f, lower_bounds, upper_bounds, parameter_dimension)
-    problem_no_jacobian = ParametricMCPs.ParametricMCP(f, lower_bounds, upper_bounds, parameter_dimension; compute_sensitivities=false, parallel=Symbolics.ShardedForm())
+    for backend in [ParametricMCPs.SymbolicUtils.SymbolicsBackend(), ParametricMCPs.SymbolicUtils.FastDifferentationBackend()]
+        rng = Random.MersenneTwister(1)
+        parameter_dimension = 2
+        # setup a dummy MCP which represents a QP with:
+        # - cost: sum((z[1:2] - θ).^2)
+        # - constaints: z[1:2] >= 0
+        f(z, θ) = [2z[1:2] - z[3:4] - 2θ; z[1:2]]
+        lower_bounds = [-Inf, -Inf, 0, 0]
+        upper_bounds = [Inf, Inf, Inf, Inf]
+        problem = ParametricMCPs.ParametricMCP(f, lower_bounds, upper_bounds, parameter_dimension; backend)
+        problem_no_jacobian = ParametricMCPs.ParametricMCP(f, lower_bounds, upper_bounds, parameter_dimension; compute_sensitivities=false, backend)
 
-    feasible_parameters = [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [rand(rng, 2) for _ in 1:10]...]
-    infeasible_parameters = -feasible_parameters
+        feasible_parameters = [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [rand(rng, 2) for _ in 1:10]...]
+        infeasible_parameters = -feasible_parameters
 
-    @testset "forward pass" begin
-        for θ in feasible_parameters
-            solution = ParametricMCPs.solve(problem, θ)
-            @test solution.z[1:2] ≈ θ
+        @testset "forward pass" begin
+            for θ in feasible_parameters
+                solution = ParametricMCPs.solve(problem, θ)
+                @test solution.z[1:2] ≈ θ
+            end
+
+            for θ in infeasible_parameters
+                solution = ParametricMCPs.solve(problem, θ)
+                @test norm(solution.z[1:2] - θ) <= norm(θ)
+            end
         end
 
-        for θ in infeasible_parameters
-            solution = ParametricMCPs.solve(problem, θ)
-            @test norm(solution.z[1:2] - θ) <= norm(θ)
-        end
-    end
+        @testset "backward pass" begin
+            function dummy_pipeline(θ)
+                solution = ParametricMCPs.solve(problem, θ)
+                sum(solution.z .^ 2)
+            end
 
-    @testset "backward pass" begin
-        function dummy_pipeline(θ)
-            solution = ParametricMCPs.solve(problem, θ)
-            sum(solution.z .^ 2)
-        end
-
-        for θ in [feasible_parameters; infeasible_parameters]
-            ∇_autodiff_reverse = only(Zygote.gradient(dummy_pipeline, θ))
-            ∇_autodiff_forward = only(Zygote.gradient(θ -> Zygote.forwarddiff(dummy_pipeline, θ), θ))
-            ∇_finitediff = FiniteDiff.finite_difference_gradient(dummy_pipeline, θ)
-            @test isapprox(∇_autodiff_reverse, ∇_finitediff; atol=1e-4)
-            @test isapprox(∇_autodiff_reverse, ∇_autodiff_forward; atol=1e-4)
-        end
-    end
-
-    @testset "missing jacobian" begin
-        function dummy_pipeline(θ, problem)
-            solution = ParametricMCPs.solve(problem, θ)
-            sum(solution.z .^ 2)
+            for θ in [feasible_parameters; infeasible_parameters]
+                ∇_autodiff_reverse = only(Zygote.gradient(dummy_pipeline, θ))
+                ∇_autodiff_forward = only(Zygote.gradient(θ -> Zygote.forwarddiff(dummy_pipeline, θ), θ))
+                ∇_finitediff = FiniteDiff.finite_difference_gradient(dummy_pipeline, θ)
+                @test isapprox(∇_autodiff_reverse, ∇_finitediff; atol=1e-4)
+                @test isapprox(∇_autodiff_reverse, ∇_autodiff_forward; atol=1e-4)
+            end
         end
 
-        @test_throws ArgumentError Zygote.gradient(θ -> dummy_pipeline(θ, problem_no_jacobian), feasible_parameters[1])
+        @testset "missing jacobian" begin
+            function dummy_pipeline(θ, problem)
+                solution = ParametricMCPs.solve(problem, θ)
+                sum(solution.z .^ 2)
+            end
+
+            @test_throws ArgumentError Zygote.gradient(θ -> dummy_pipeline(θ, problem_no_jacobian), feasible_parameters[1])
+        end
     end
 end
