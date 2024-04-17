1ff4e72006db9835eac0b7c512ab43f3c5c569e3
lassepe <lasse.peters@mailbox.org>
2024-04-17 10:25:10 +0200

    Update constructor docstring

--- src/parametric_problem.jl
+++ src/parametric_problem.jl
@@ -2,28 +2,6 @@
 ParametricMCP represents a mixed complementarity problem (MCP) parameterized by some vector `θ`.
 
 Solutions of this problem solve f(z, θ) ⟂ lb ≤ z ≤ ub.
-
---------------------------------------------------------------------------------
-
-The main constructor for compiling a `ParametricMCP` from
-
-Positional arguments:
-- `f`: callable as `f(z, θ)` that maps a length `n` vector of decision variables `z` and a \
-parameter vector `θ` of size `parameter_dimension` to an length `n` vector output.
-- `lower_bounds`: A length `n` vector of element-wise lower bounds on the decision variables `z`.
-- `upper_bounds`: A length `n` vector of element-wise upper bounds on the decision variables `z`.
-- `parameter_dimension`: the size of the parameter vector `θ` in `f`.
-
-Keyword arguments:
-- `[backend]`: the backend to be used for compiling callbacks for `f` and its Jacobians needed by PATH. `SymbolicsBackend` (default) is slightly more flexible. `FastDifferentationBackend` has reduced compilation times and reduced runtime in some cases.
-- `compute_sensitivities`: whether to compile the callbacks needed for sensitivity computation.
-- `[problem_size]`: the number of decision variables. If not provided and `lower_bounds` or `upper_bounds` are vectors, the problem size is inferred from the length of these vectors.
-
-Note, this constructor uses `Symbolics.jl` to compile the relevant low-level functions. Therefore,
-`f` must be implemented in a sufficiently generic way that supports symbolic evaluation. In cases
-where that is strictly infeasible, you can still use the low-level constructor to generate a
-`ParametricMCP`. In general, however, the use of this convenience constructor is advised.
-
 """
 struct ParametricMCP{T1,T2,T3}
     "A callable `f!(result, z, θ)` which stores the result of `f(z, θ)` in `result`"
@@ -49,6 +27,29 @@ get_problem_size(problem::ParametricMCP) = problem.problem_size
 get_parameter_dimension(problem::ParametricMCP) = problem.parameter_dimension
 
 # This method allows the user to specify the backend via a keyword argument which defaults to using Symbolics.jl
+
+"""
+--------------------------------------------------------------------------------
+
+The main constructor for compiling a `ParametricMCP` from
+
+Positional arguments:
+- `f`: callable as `f(z, θ)` that maps a length `n` vector of decision variables `z` and a \
+parameter vector `θ` of size `parameter_dimension` to an length `n` vector output.
+- `lower_bounds`: A length `n` vector of element-wise lower bounds on the decision variables `z`.
+- `upper_bounds`: A length `n` vector of element-wise upper bounds on the decision variables `z`.
+- `parameter_dimension`: the size of the parameter vector `θ` in `f`.
+
+Keyword arguments:
+- `[backend]`: the backend (from `ParametricMCPs.SymbolicUtils`) to be used for compiling callbacks for `f` and its Jacobians needed by PATH. `SymbolicsBackend` (default) is slightly more flexible. `FastDifferentationBackend` has reduced compilation times and reduced runtime in some cases.
+- `compute_sensitivities`: whether to compile the callbacks needed for sensitivity computation.
+- `[problem_size]`: the number of decision variables. If not provided and `lower_bounds` or `upper_bounds` are vectors, the problem size is inferred from the length of these vectors.
+
+Note, this constructor uses symbolic tracing to compile the relevant low-level functions. Therefore,
+`f` must be implemented in a sufficiently generic way that supports symbolic evaluation. In cases
+where that is infeasible or impractical, you can still use the low-level constructor to generate a
+`ParametricMCP`. In general, however, the use of this convenience constructor is advised.
+"""
 function ParametricMCP(
     f,
     lower_bounds,
